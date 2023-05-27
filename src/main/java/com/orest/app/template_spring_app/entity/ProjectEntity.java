6d70dcf1a9020c306cbc92a0a763861383e7d973
Orest Sonich <sonich.work@gmail.com>
2023-05-27 14:10:00 +0300

    123

---
+++ src/main/java/com/orest/app/template_spring_app/entity/ProjectEntity.java
@@ -0,0 +1,23 @@
+package com.orest.app.template_spring_app.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProjectEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private int hours;
+    private String projectName;
+    private String user;
+
+}
