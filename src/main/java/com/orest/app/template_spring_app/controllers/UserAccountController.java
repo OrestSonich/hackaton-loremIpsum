ad2a51348b836d65911edd7557f09e5cdc623b35
Orest Sonich <sonich.work@gmail.com>
2023-05-27 19:07:14 +0300

    na

--- src/main/java/com/orest/app/template_spring_app/controllers/UserAccountController.java
+++ src/main/java/com/orest/app/template_spring_app/controllers/UserAccountController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
 @CrossOrigin
 public class UserAccountController {
@@ -15,8 +15,7 @@ public class UserAccountController {
     @Autowired
     private UserService service;
 
-    @CrossOrigin
-    @GetMapping("/user/{username}")
+    @GetMapping("/{username}")
     public ResponseEntity getUserById(@PathVariable String username){
         return ResponseEntity.ok(service.getOneByEmail(username));
     }
