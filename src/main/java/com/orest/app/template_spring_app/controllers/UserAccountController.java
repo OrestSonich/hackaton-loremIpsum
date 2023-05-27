c908b1ea8d2ab6721d5fec8b7573a365789ed616
Orest Sonich <sonich.work@gmail.com>
2023-05-27 17:04:54 +0300

    test deploy

--- src/main/java/com/orest/app/template_spring_app/controllers/UserAccountController.java
+++ src/main/java/com/orest/app/template_spring_app/controllers/UserAccountController.java
@@ -2,17 +2,20 @@ package com.orest.app.template_spring_app.controllers;
 
 import com.orest.app.template_spring_app.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api/v1/")
 @RequiredArgsConstructor
 @CrossOrigin
 public class UserAccountController {
 
+    @Autowired
     private UserService service;
-    @GetMapping
+
+    @GetMapping("/user")
     public ResponseEntity getUserById(@RequestParam Long user_id){
         return ResponseEntity.ok(service.getOneById(user_id));
     }
