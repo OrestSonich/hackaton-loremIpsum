763e8ec878c41bb484a399a80efee00fd1822880
maksfedyna <fedyna.maksym.andriovych@gmail.com>
2023-05-27 17:00:25 +0300

    123

---
+++ src/main/java/com/orest/app/template_spring_app/entity/RangEntity.java
@@ -0,0 +1,28 @@
+package com.orest.app.template_spring_app.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class RangEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String rang;
+    private String user;
+
+    @ManyToOne
+    @JoinColumn(name = "rang_id")
+    private UserEntity owner;
+    private Date createdAt;
+    private Date updatedAt;
+}
---
+++ src/main/java/com/orest/app/template_spring_app/services/RangService.java
@@ -0,0 +1,45 @@
+package com.orest.app.template_spring_app.services;
+
+import com.orest.app.template_spring_app.entity.ProjectEntity;
+import com.orest.app.template_spring_app.entity.RangEntity;
+import com.orest.app.template_spring_app.pojo.ProjectModel;
+import com.orest.app.template_spring_app.pojo.RangModel;
+import com.orest.app.template_spring_app.repository.RangRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+@Service
+public class RangService {
+
+    @Autowired
+    private RangRepo rangRepo;
+
+    public RangModel addCosts(RangEntity rangs, Long id) {
+        rangs.setCreatedAt(Date.valueOf(LocalDate.now(ZoneId.of("GMT+0300"))));
+        RangEntity rang = rangRepo.findById(id).
+                orElseThrow();
+        rangs.setOwner(rangs.getOwner());
+        return RangModel.toModel(rangRepo.save(rangs));
+    }
+
+    public void deleteRang(Long id) {
+        rangRepo.deleteById(id);
+    }
+
+    public RangModel updateRang(RangEntity rangs, Long id) {
+        RangEntity findEntity = rangRepo.findById(id)
+                .orElseThrow();
+        if (rangs.getRang() != null)
+            findEntity.setRang(rangs.getRang());
+        if (rangs.getUser() != null)
+            findEntity.setUser(rangs.getUser());
+
+        findEntity.setUpdatedAt(Date.valueOf(LocalDate.now(ZoneId.of("GMT+0300"))));
+        rangRepo.save(findEntity);
+        return RangModel.toModel(findEntity);
+    }
+}
